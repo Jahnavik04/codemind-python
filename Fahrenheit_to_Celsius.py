0357408456b3fe23f7a70b8deba39cf60a80e5c3
Jahnavi Kamidi <97437419+Jahnavik04@users.noreply.github.com>
2024-02-16 10:50:25 +0530

    f=float(input()) c=((f-32)*5)/9 print("%.2f"%c)

--- Fahrenheit_to_Celsius.py
+++ Fahrenheit_to_Celsius.py
@@ -1,6 +1,3 @@
-def fahrenheit_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * 5/9
-    return celsius
-fahrenheit = float(input())
-celsius = fahrenheit_to_celsius(fahrenheit)
-print("{:.2f}".format(celsius))
+f=float(input())
+c=((f-32)*5)/9
+print("%.2f"%c)
